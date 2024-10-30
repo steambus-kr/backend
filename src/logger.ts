ae95d963917a99c7603e9e966a2a9a663a63f1fc
p-sw <shinwoo.park@psw.kr>
2024-10-30 22:22:50 +0900

    feat: add logger multistream to support log to file

--- src/logger.ts
+++ src/logger.ts
@@ -1,5 +1,31 @@
-import { createPinoLogger } from "@bogeychan/elysia-logger";
+import { createPinoLogger, pino } from "@bogeychan/elysia-logger";
+import { join } from "path";
+import { createWriteStream } from "fs";
+
+const logRoot = process.env.LOG_ROOT ?? "logs";
+const debugStream = createWriteStream(join(logRoot, "debug.stream.out"));
+const logStream = createWriteStream(join(logRoot, "log.stream.out"));
+const errorStream = createWriteStream(join(logRoot, "error.stream.out"));
+
+const stream = pino.multistream([
+  {
+    level: "debug",
+    stream: debugStream,
+  },
+  {
+    stream: logStream,
+  },
+  {
+    level: "error",
+    stream: errorStream,
+  },
+  {
+    level: "fatal",
+    stream: errorStream,
+  },
+]);
 
 export const logger = createPinoLogger({
   transport: { target: "pino-pretty" },
+  stream,
 });
