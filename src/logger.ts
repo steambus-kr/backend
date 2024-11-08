bf3eb718a0d87b97e54cfd27d4297863b6f24681
p-sw <shinwoo.park@psw.kr>
2024-11-08 13:14:33 +0900

    feat: improve logging & add pcLogger

--- src/logger.ts
+++ src/logger.ts
@@ -11,6 +11,9 @@ if (!existsSync(logRoot)) {
 const logStream = createWriteStream(join(logRoot, "log.stream.out"));
 const errorStream = createWriteStream(join(logRoot, "error.stream.out"));
 const fgiStream = createWriteStream(join(logRoot, "fgi.stream.out"));
+const fgiErrorStream = createWriteStream(join(logRoot, "fgi.error.stream.out"));
+const pcStream = createWriteStream(join(logRoot, "pc.stream.out"));
+const pcErrorStream = createWriteStream(join(logRoot, "pc.error.stream.out"));
 
 const stream = pino.multistream([
   {
@@ -40,9 +43,32 @@ export const fgiLogger = createPinoLogger({
       stream: fgiStream,
     },
     {
+      level: "warn",
+      stream: fgiErrorStream,
+    },
+    {
       level: "info",
       stream: pretty({ colorize: true }),
     },
   ]),
-  name: "FGI",
+  name: "FetchGameInfoService",
+});
+
+export const pcLogger = createPinoLogger({
+  level: "debug",
+  stream: pino.multistream([
+    {
+      level: "debug",
+      stream: pcStream,
+    },
+    {
+      level: "warn",
+      stream: pcErrorStream,
+    },
+    {
+      level: "info",
+      stream: pretty({ colorize: true }),
+    },
+  ]),
+  name: "PlayerCountService",
 });
