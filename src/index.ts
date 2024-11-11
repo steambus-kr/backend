7c4c243108bfdc652f26f5030ee2b075ae3d2828
p-sw <shinwoo.park@psw.kr>
2024-11-11 19:24:10 +0900

    feat: add server health check endpoint

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,9 @@ config();
 const app = new Elysia({ prefix: "/api" })
   .use(swagger)
   .use(cron)
+  .get("/health", async () => {
+    return { ok: true };
+  })
   .listen(process.env.PORT ?? 3000);
 
 console.log(
