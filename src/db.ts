95f77ba99d7eb19b047c714416aafbe39f5c435d
p-sw <shinwoo.park@psw.kr>
2024-11-12 23:59:42 +0900

    refactor: remove log db idea

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,3 @@
 import { PrismaClient } from "@prisma/client";
-import Database from "bun:sqlite";
 
 export const db = new PrismaClient();
-export const logdb = new Database("logdb.sqlite", { create: true });
-logdb.exec("PRAGMA journal_mode = WAL;");
-logdb.run(`
-CREATE TABLE IF NOT EXISTS LogFile (
-  path TEXT,
-  lastWrite INTEGER
-);
-`);
