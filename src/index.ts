7b6d6e17b578d9198629d0cc3889789fc6b70022
p-sw <shinwoo.park@psw.kr>
2024-11-12 16:18:53 +0900

    refactor: change start log to logger

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,4 @@ const app = new Elysia({ prefix: "/api" })
   })
   .listen(process.env.PORT ?? 3000);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
-);
+logger.info(`Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
