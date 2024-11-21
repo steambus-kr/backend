df868b3b6a6b2a301089d60fa5bdc4e935e4f029
p-sw <shinwoo.park@psw.kr>
2024-11-21 15:09:49 +0900

    Refactor and enhance game recommendation service
    Added new utility function `calculateRatio` and updated code to improve consistency and readability. Refined `GameService` class and `getGameInfo` method for better type safety and data processing efficiency.

--- src/services/recommend.service.ts
+++ src/services/recommend.service.ts
@@ -1,18 +1,19 @@
-import {RecommendFilter} from "@/types";
-import {db} from "@/db";
-import {Prisma} from "@prisma/client";
+import { RecommendFilter } from "@/types";
+import { db } from "@/db";
+import { Prisma } from "@prisma/client";
+import { calculateRatio } from "@/utils";
 
 export class RecommendService {
   filter: Partial<RecommendFilter>;
   where: {
-    query: Prisma.GameWhereInput,
+    query: Prisma.GameWhereInput;
     // filter: ((o: NonNullable<Awaited<ReturnType<typeof GameService.prototype.getGameInfo>>>) => boolean)[],
     // refine: (<R extends NonNullable<Awaited<ReturnType<typeof GameService.prototype.getGameInfo>>>, T extends keyof R>(o: R) => [T, R[T]])[]
   };
 
   constructor(filter: Partial<RecommendFilter>) {
     this.filter = filter;
-    this.where = this.buildFilter()
+    this.where = this.buildFilter();
   }
 
   async updateFilter(filter: Partial<RecommendFilter>) {
@@ -60,25 +61,29 @@ export class RecommendService {
     if (this.filter.genre) {
       filterObj.genres = {
         some: {
-          genre_name: this.filter.genre
-        }
-      }
+          genre_name: this.filter.genre,
+        },
+      };
     }
 
-    return { query: filterObj, /* refine: refineFunctions, filter: filterFunctions */ };
+    return {
+      query: filterObj /* refine: refineFunctions, filter: filterFunctions */,
+    };
   }
 
   async getAppList() {
-    return db.game.findMany({
-      where: this.where.query,
-      select: {
-        app_id: true,
-      }
-    }).then((r) => r.map((v) => v.app_id));
+    return db.game
+      .findMany({
+        where: this.where.query,
+        select: {
+          app_id: true,
+        },
+      })
+      .then((r) => r.map((v) => v.app_id));
   }
 
   async selectRandom(appids: number[], exclude: number[]) {
-    const filteredAppIds = appids.filter(appid => !exclude.includes(appid));
+    const filteredAppIds = appids.filter((appid) => !exclude.includes(appid));
 
     if (filteredAppIds.length === 0) {
       return null;
@@ -89,35 +94,104 @@ export class RecommendService {
   }
 }
 
-class GameService {
-  refinedGameInfo?: {
-    app_id: number;
-    title: string;
-    description: string;
-    owner_min: number;
-    release_date: string;
-    player_count: {
-      latest: number;
-      peak: number;
-    };
-    thumbnail_src: string;
-    review_positive: number;
-    review_negative: number;
-    review_ratio?: number;
-    genres: string[];
+interface IRefinedGameInfo {
+  app_id: number;
+  title: string;
+  description: string;
+  owner_min: number;
+  release_date: string;
+  player_count: {
+    latest: {
+      count: number;
+      date: number;
+    } | null;
+    peak: {
+      count: number;
+      date: number;
+    } | null;
   };
+  thumbnail_src: string;
+  review: {
+    positive: number;
+    negative: number;
+    ratio: number;
+  };
+  genres: string[];
+}
 
+// TS 씨발련 ㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗㅗ
+// 마소 개새끼들아 일좀해
+// 아오 씨발 걍
+type RequiredWithoutUndefined<T> = {
+  [K in keyof T]-?: T[K] extends infer R | undefined ? R : never;
+};
+
+class GameService {
   constructor(private readonly gameId: number) {}
 
-  async getGameInfo() {
-    return db.game.findUnique({
+  async getGameInfo(): Promise<IRefinedGameInfo | null> {
+    const game = await db.game.findUnique({
       where: {
         app_id: this.gameId,
       },
       include: {
         genres: true,
-        player_count: true
-      }
+        player_count: true,
+      },
     });
+
+    if (!game) {
+      return null;
+    }
+
+    const obj_player_count = game.player_count.reduce(
+      (p, c) => {
+        if (!p.latest || !p.peak) return { latest: undefined, peak: undefined };
+        // TS 씨발련 ㅋㅋ
+        const n = { ...(p as RequiredWithoutUndefined<typeof p>) };
+        if (c.date.getTime() > n.latest.date.getTime()) {
+          n.latest = c;
+        }
+        if (c.count > n.peak.count) {
+          n.peak = c;
+        }
+        return n;
+      },
+      {
+        latest: game.player_count.at(0),
+        peak: game.player_count.at(0),
+      },
+    );
+
+    return {
+      app_id: game.app_id,
+      title: game.title,
+      description: game.description,
+      review: {
+        positive: game.review_positive,
+        negative: game.review_negative,
+        ratio:
+          game.review_ratio ??
+          calculateRatio(game.review_positive, game.review_negative),
+      },
+      release_date: game.release_date,
+      thumbnail_src: game.thumbnail_src,
+      owner_min: game.owner_count,
+      player_count: {
+        latest: obj_player_count.latest
+          ? {
+              count: obj_player_count.latest.count,
+              date: obj_player_count.latest.date.getTime(),
+            }
+          : null,
+        peak: obj_player_count.peak
+          ? {
+              count: obj_player_count.peak.count,
+              date: obj_player_count.peak.date.getTime(),
+            }
+          : null,
+      },
+      genres: game.genres.map(({ genre_name }) => genre_name),
+    };
   }
 }
